2e51c3dcc34abd07377657619472d8a7aab73f89
Felo Vilches <felovilches@gmail.com>
2021-09-18 06:33:23 +0900

    revamp webpack.

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,7 +1,7 @@
 var appName = require('../package.json').name;
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const ASSETS_FOLDER = path.resolve(__dirname, '../src', 'assets');
 const DIST_FOLDER = path.resolve(__dirname, '../dist');
@@ -10,21 +10,27 @@ module.exports = {
   name: appName,
   entry: [
     path.join(ASSETS_FOLDER, './app.js'),
-    path.join(ASSETS_FOLDER, './styles.css')
+    path.join(ASSETS_FOLDER, './styles.scss')
   ],
   output: {
     path: DIST_FOLDER,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    clean: true
   },
   resolve: {
-    extensions: ['.js'],
+    extensions: ['.js', '.scss', '.css'],
     alias: {
+      '@node_modules': path.resolve(__dirname, '../node_modules'),
       '@styles': path.join(ASSETS_FOLDER, 'stylesheets'),
       '@js': path.join(ASSETS_FOLDER, 'javascripts')
     }
   },
   module: {
     rules: [
+      {
+        test: /\.(scss)$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
       {
         test: /\.(css)$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
